35b40ff67bbd4c53b1ec067e7fea74aa799f1221
William Shiao <willshiao@gmail.com>
2021-05-28 18:15:37 -0700

    Use compact SVD

--- corcondia/corcondia.py
+++ corcondia/corcondia.py
@@ -9,32 +9,29 @@ def kronecker_mat_ten(matrices, X):
         X = mode_dot(X, M, k)
     return X
 
+# Shortcut to invert singular values.
+# Given a vector of singular values, returns the inverted matrix
+def invert_sing(s):
+    return np.diag(1.0 / s[::-1])
+
 def corcondia_3d(X, k = 1, init='random', **kwargs):
     weights, X_approx_ks = parafac(X, k, init=init, **kwargs)
 
     A, B, C = X_approx_ks
     x = tl.cp_to_tensor((weights, X_approx_ks))
 
-    Ua, Sa, Va = np.linalg.svd(A)
-    Ub, Sb, Vb = np.linalg.svd(B)
-    Uc, Sc, Vc = np.linalg.svd(C)
+    Ua, Sa, Va = np.linalg.svd(A, full_matrices=False)
+    Ub, Sb, Vb = np.linalg.svd(B, full_matrices=False)
+    Uc, Sc, Vc = np.linalg.svd(C, full_matrices=False)
 
-    SaI = np.zeros((Ua.shape[0], Va.shape[0]), float)
-    np.fill_diagonal(SaI, Sa)
-
-    SbI = np.zeros((Ub.shape[0], Vb.shape[0]), float)
-    np.fill_diagonal(SbI, Sb)
-
-    ScI = np.zeros((Uc.shape[0], Vc.shape[0]), float)
-    np.fill_diagonal(ScI, Sc)
-
-    SaI = np.linalg.pinv(SaI)
-    SbI = np.linalg.pinv(SbI)
-    ScI = np.linalg.pinv(ScI)
+    inverted = [invert_sing(x) for x in (Sa, Sb, Sc)]
 
     part1 = kronecker_mat_ten([Ua.T, Ub.T, Uc.T], x)
-    part2 = kronecker_mat_ten([SaI, SbI, ScI], part1)
+    part2 = kronecker_mat_ten(inverted, part1)
     G = kronecker_mat_ten([Va.T, Vb.T, Vc.T], part2)
+
+    for i in range(k):
+        G[:,:,i] = G[:,:,i] / G[i,i,i]
 
     T = np.zeros((k, k, k))
     for i in range(k):
